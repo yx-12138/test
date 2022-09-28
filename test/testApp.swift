637fa8fa1b4ed9362e1259cd6e256ae465ca5326
yx <1642583417@qq.com>
2022-09-28 20:35:39 +0800

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by 付勇星 on 2022/9/28.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
